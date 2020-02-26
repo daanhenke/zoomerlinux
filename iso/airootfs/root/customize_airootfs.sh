64212d9a87ba70266361a6d6b88cb8246a858734
Daan Henke <daan@daan.vodka>
2020-02-26 20:54:46 +0100

    Commented airootfs script and added setup pacman keyring

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -2,6 +2,15 @@
 
 set -e -u
 
+USER_NAME=live
+USER_PASS=zoomer
+ROOT_PASS=zoomer
+
+usermode()
+{
+    runuser -l $USER_NAME -c "$@"
+}
+
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
@@ -11,12 +20,17 @@ usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 -R /root
 
+# Fix sudoers.d permissions
 chown root:root -R /etc/sudoers.d
 
-useradd -m -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh live
-echo live:zoomer | chpasswd
-echo root:zoomer | chpasswd
-chown -R live /home/live
+# Do user initialization
+if ! id -u $USER_NAME > /dev/null 2>&1
+then
+    useradd -m -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh $USER_NAME
+fi
+echo $USER_NAME:$USER_PASS | chpasswd
+echo root:$ROOT_PASS | chpasswd
+chown -R $USER_NAME /home/$USER_NAME
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
@@ -28,5 +42,11 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 
+# Enable lightdm on boot
 systemctl set-default graphical.target
 systemctl enable lightdm.service
+
+# Setup pacman
+pacman-key --init
+pacman-key --populate
+
