7cd155ca8f60e2152d91ba28cbf409317c52d61a
Daan Henke <daan@daan.vodka>
2020-02-26 21:03:24 +0100

    Fixed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ useradd builder
 
 # Build the ISO file
 cd /iso
-runuser -l builder -c 'aur.sh'
-build.sh -v
+runuser -l builder -c './aur.sh'
+./build.sh -v
 
 # Copy ISO to output volume
 cp /iso/out/* /dist
